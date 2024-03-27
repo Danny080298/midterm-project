d059c3ac3416b119575167b881fb5ad907feb08e
Danny Nguyen <dqngyen1@memphis.edu>
2024-03-27 11:27:08 -0500

    add kinds of cards with different suits and values, with test unit and rspec to test it out

--- lib/cardpoker.rb
+++ lib/cardpoker.rb
@@ -1,4 +1,30 @@
 class Card
-   suit = "Spade"
-   value = "2"
+    attr_reader :suit, :value
+
+    def initialize(suit, value)
+        @suit, @value = suit, value
+    end
+    def to_s
+        "#{@value} of #{@suit}"
+    end
+
+end
+
+
+class Deck
+    SUITS = %w[Hearts Diamonds Clubs Spades]
+    VALUES = %w[2 3 4 5 6 7 8 9 10 Jack Queen King Ace]
+    attr_reader :cards
+
+    def initialize
+        @cards = SUITS.product(VALUES).map { |suit, value| Card.new(suit, value) }
+    end
+end
+
+
+deck = Deck.new
+if deck.cards.length == 52
+    puts "let play"
+else
+    puts "Get a new deck"
 end
--- spec/cardpoker_spec.rb
+++ spec/cardpoker_spec.rb
@@ -2,10 +2,24 @@ require 'spec_helper'
 require_relative '../lib/cardpoker'
 
 RSpec.describe Card do
-    it "suit" do 
-        expect(suit).to eq("Spade")
-        expect(value).to eq("2")
+    it "have value of suit" do 
+        card = Card.new('Spade', 2)
+        expect(card.suit).to eq("Spade")
+        expect(card.value).to eq(2)
     end
     
-    
+    it "have value of suit" do 
+        card = Card.new('Diamond', 4)
+        expect(card.suit).to eq("Diamond")
+        expect(card.value).to eq(4)
+    end
+
+end
+RSpec.describe Deck do
+    describe "#intialize" do
+        it "have 52 cards" do
+            @deck = Deck.new
+            expect(@deck.instance_variable_get(:@cards).count).to eq(52)
+        end
+    end
 end
