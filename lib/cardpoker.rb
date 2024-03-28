c34f653b93e6c1b6af9828ba204eae16c5485096
Danny Nguyen <dqngyen1@memphis.edu>
2024-03-28 12:28:24 -0500

    working on high card function, adding variables and comparasion check when players's hand tie and checking on higher value card

--- lib/cardpoker.rb
+++ lib/cardpoker.rb
@@ -40,8 +40,57 @@ class Hand
         card_descriptions = @cards.map { |card| "#{card.value} of #{card.suit}" }.join(', ')
         "Hand contains: #{card_descriptions}"
     end
+    
+    RANKING = {
+        'royal_flush' => 10,
+        'straight_flush' => 9,
+        'four_of_a_kind' => 8,
+        'full_house' => 7,
+        'flush' => 6,
+        'straight' => 5,
+        'three_of_a_kind' => 4,
+        'two_pair' => 3,
+        'pair' => 2,
+        'high_card' => 1
+    }.freeze
+
+    def <=>(other_hand)
+        check_hand = RANKING[best_rank] <=> RANKING[other_hand.best_rank]
+        return check_hand unless check_hand == 0
+
+        case best_rank
+        when 'pair','two_pair','three_of_a_kind','four_of_a_kind'
+        
+        when 'straight','straight_flush','high_card'
+            
+        when 'flush', 'full_house'
+
+    end
+    def group_cards_by_value
+        @cards.group_by(&:value)
+    end
+    def compare_high_card(other_hand)
+        our_kickers = @cards.reject { |card| group_cards_by_value[card.value].size > 1 }.max_by { |card| card_rank(card.value) }
+        other_kickers = other_hand.cards.reject { |card| other_hand.group_cards_by_value[card.value].size > 1 }.max_by { |card| card_rank(card.value) }
+    
+        card_rank(our_kickers.value) <=> card_rank(other_kickers.value)
+    end
+
 
     #best rank
+    def best_rank
+        return 'royal_flush' if royal_flush?
+        return 'straight_flush' if straight_flush?
+        return 'four_of_a_kind' if four_of_a_kind?
+        return 'full_house' if full_house?
+        return 'flush' if flush?
+        return 'straight' if straight?
+        return 'three_of_a_kind' if three_of_a_kind?
+        return 'two_pair' if two_pair?
+        return 'pair' if pair?
+        'high_card'
+    end
+
     #royal straight flush
     def royal_flush?
         flush? && straight? && @cards.map { |card| card_value(card.value) }.include?(14) && @cards.map { |card| card_value(card.value) }.min == 10
@@ -91,8 +140,11 @@ class Hand
         value_counts = @cards.each_with_object(Hash.new(0)) { |card, counts| counts[card.value] += 1 }
         value_counts.any? { |_, count| count == 2 }
     end
-    #high card
 
+    #high card
+    def high_card
+        @cards.max_by { |card| card_rank(card.value) }
+    end
     def card_value(value)
         ranks = {'2' => 2, '3' => 3, '4' => 4, '5' => 5, 
         '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 
